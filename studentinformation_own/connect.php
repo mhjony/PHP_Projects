c386fb8abc6e1f828936524487800efe7b0bf169
Nepsun <mahmudul.jhony@gmail.com>
2015-08-11 15:48:28 +0600

    New echo line is added

--- studentinformation_own/connect.php
+++ studentinformation_own/connect.php
@@ -9,6 +9,21 @@
 		mysql_connect("localhost","root","") or die (mysql_error());
 		mysql_select_db ("test_db");
 		//echo "Connected";
+		//echo "Connected";
+		//echo "Connected";
+		//echo "Connected";
+		//echo "Connected";
+		//echo "Connected";
+		//echo "Connected";
+		//echo "Connected";
+		//echo "Connected";
+		//echo "Connected";
+		//echo "Connected";
+		//echo "Connected";
+		//echo "Connected";
+		//echo "Connected";
+		//echo "Connected";
+		//echo "Connected";
 	?>
 
 </body>
